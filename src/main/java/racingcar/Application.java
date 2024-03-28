e79235684df671671cc96e0c8c7088f69c0ca7b4
신관규 <soundbar91@gmail.com>
2024-03-28 21:25:03 +0900

    feat: 시도 회수 입력 기능

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,6 +8,10 @@ public class Application {
         try {
             System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
             String[] carsName = carNameCheck(readLine().split(","));
+
+            System.out.println("시도할 회수는 몇회인가요?");
+            int gameTry = Integer.parseInt(readLine());
+
         } catch (IllegalAccessException e) {
             System.out.print("애플리케이션을 종료합니다.");
         }
