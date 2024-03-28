f732ca434181d2997da8cdec20bc1b5813622ed0
신관규 <soundbar91@gmail.com>
2024-03-28 21:23:12 +0900

    feat: 자동차 이름 예외 처리 기능

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,8 +5,23 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+            String[] carsName = carNameCheck(readLine().split(","));
+        } catch (IllegalAccessException e) {
+            System.out.print("애플리케이션을 종료합니다.");
+        }
+    }
 
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String[] carsName = readLine().split(",");
+    public static String[] carNameCheck(String[] carsName) throws IllegalAccessException {
+        if (carsName.length < 2)
+            throw new IllegalAccessException("최소 2개의 자동차가 참여해야 합니다.");
+
+        for (String carName : carsName){
+            if (carName.length() > 5) {
+                throw new IllegalAccessException("자동차 이름의 길이는 5자 이하만 가능합니다.");
+            }
+        }
+        return carsName;
     }
 }
