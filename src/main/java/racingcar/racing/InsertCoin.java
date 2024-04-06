25d9e47347e03732ad39d56391c81ad3d7352ad0
신관규 <soundbar91@gmail.com>
2024-04-06 16:42:07 +0900

    feat: InsertCoin 클래스 추가

---
+++ src/main/java/racingcar/racing/InsertCoin.java
@@ -0,0 +1,28 @@
+package racingcar.racing;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class InsertCoin {
+
+    public int inputTryNumber() {
+        try {
+            System.out.println("시도할 회수는 몇회인가요?");
+            return gameTryCheck(readLine());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            System.out.println("애플리케이션을 종료합니다.");
+            throw e;
+        }
+    }
+
+    public int gameTryCheck(String input) throws IllegalArgumentException {
+        try {
+            int coin = Integer.parseInt(input);
+            if (coin < 0) throw new IllegalArgumentException("시도 회수는 음수가 될 수 없습니다.");
+
+            return coin;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자를 입력하세요.");
+        }
+    }
+}
